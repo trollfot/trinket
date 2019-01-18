54ee2d79fa079e2f0fb5ce1f0561e0303b0b4d27
Souheil CHELFOUH <trollfot@gmail.com>
2019-01-18 20:22:34 +0100

    Preparing 0.1.2

--- docs/HISTORY.rst
+++ docs/HISTORY.rst
@@ -2,12 +2,16 @@
 Changelog
 *********
 
+0.1.2 (2019-01-18)
+==================
+
+* Updated distribution files
+
 0.1.1 (2019-01-18)
 ==================
 
 * Added tests and testing utilities
 * Fixed missing imports
-
 
 0.1 (2019-01-17)
 ================
--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ tests_require = [
     ]
 
 setup(name='trinket',
-      version='0.1.1',
+      version='0.1.2',
       description="Curio-based web framework",
       long_description="%s\n\n%s" % (
           read('README.rst'), read(path.join('docs', 'HISTORY.rst'))),
